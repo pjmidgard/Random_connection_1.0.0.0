d8d565a73918ce295f18681deb9f2c21679ab535
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-02-21 19:41:10 +0000

    Update Random_connection_1.0.0.0.py

--- Random_connection_1.0.0.0.py
+++ Random_connection_1.0.0.0.py
@@ -172,7 +172,7 @@ class encypthion_class:
                                     lenf3=len(size_data2)
                                 lenf2=len(size_data2)
                                 cout_compress=0
-                                while cout_compress!=2**16:
+                                while cout_compress!=255:
                                    
 	                                
 	                                cout_compress+=1
@@ -243,7 +243,7 @@ class encypthion_class:
 	                                                           while Times2!=76:
 	                                                                       Times2+=4
 	                                                                       if Times>=0 and Times2>=0 and Calculus[Times:Times+4]!=Calculus[Times2:Times2+4] and Times2!=Times:
-	                                                                       	Number_Times=(Times//4)-(Times2//4)
+	                                                                       	Number_Times=((Times//4)-(Times2//4))-1
 	                                                                       	Change=Calculus[Times:Times+4]
 	                                                                       	Equal_Not_compress=format(Number_Times,'04b')
 	                                                                       	
@@ -264,7 +264,7 @@ class encypthion_class:
 	                                                                       		compress6=1                                                                   		
 	
 	                                                                        if Times>=0 and Times2>=0 and Calculus[Times:Times+4]==Calculus[Times2:Times2+4] and Times2!=Times:
-	                                                                                       Number_Times=(Times//4)-(Times2//4)
+	                                                                                       Number_Times=((Times//4)-(Times2//4))-1
 	                                                                                       if Number_Times>0 and Number_Times<=7 and compress_start==0:
 	                                                                                       	save_01=Calculus[:Times2+3]+Calculus[Times2+4:80]
 	                                                                                       	Equal_Not_compress=format(Number_Times,'03b')
@@ -308,7 +308,7 @@ class encypthion_class:
 	                                                      while Times4!=156:
 	                                                            Times4+=4
 	                                                            if Times3>=80 and Times4>=80 and Calculus[Times3:Times3+4]!=Calculus[Times4:Times4+4] and Times4!=Times3:
-	                                                                       	Number_Times=(Times3//4)-(Times4//4)
+	                                                                       	Number_Times=((Times3//4)-(Times4//4))-1
 	                                                                       	Change=Calculus[Times3:Times3+4]
 	                                                                       	Equal_Not_compress=format(Number_Times,'04b')
 	                                                                       	
@@ -328,7 +328,7 @@ class encypthion_class:
 	                                                                       		compress5=1
 	                                                                       		compress7=1	                                                                       		
 	                                                            if Times3>=80 and Times4>=80 and Calculus[Times3:Times3+4]==Calculus[Times4:Times4+4] and Times4!=Times3:
-	                                                               Number_Times2=(Times3//4)-(Times4//4)
+	                                                               Number_Times2=((Times3//4)-(Times4//4))-1
 	                                                              
 	                                                              
 	                                                             
@@ -385,7 +385,7 @@ class encypthion_class:
 	                                
 	                                
 	                                
-	                                size_data2=size_data12
+	                                size_data2="0"+size_data12
                                 
                                 size_data11="1"+size_data12
                                 
