235202b0f104942e253b4e5bc69b6525004ff910
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-02-25 11:59:40 +0000

    Update Random_connection_1.0.0.0.py

--- Random_connection_1.0.0.0.py
+++ Random_connection_1.0.0.0.py
@@ -172,7 +172,7 @@ class encypthion_class:
                                     lenf3=len(size_data2)
                                 lenf2=len(size_data2)
                                 cout_compress=0
-                                while cout_compress!=500:
+                                while cout_compress!=1000:
                                     cout_compress+=1
                                     size_data12=""
                                     Times=""
@@ -215,7 +215,7 @@ class encypthion_class:
                                             while Times2!=76:
                                                 Times2+=4
                                                 if Times>=0 and Times2>=0 and Calculus[Times:Times+4]!=Calculus[Times2:Times2+4] and Times2!=Times:
-                                                    Number_Times=(Times//4)-(Times2//4)
+                                                    Number_Times=((Times//4)-(Times2//4))-1
                                                     Change=Calculus[Times:Times+4]
                                                     Equal_Not_compress=format(Number_Times,'04b')
                                                     if Calculus[Times2:Times2+4]==Equal_Not_compress and Calculus[Times2+4:Times2+5]==Equal_Not_compress[3:4]:
@@ -226,7 +226,7 @@ class encypthion_class:
                                                         compress4=1
                                                         compress6=1
                                                 if Times>=0 and Times2>=0 and Calculus[Times:Times+4]==Calculus[Times2:Times2+4] and Times2!=Times:
-                                                    Number_Times=(Times//4)-(Times-Times2//4)
+                                                    Number_Times=((Times//4)-(Times-Times2//4))-1
                                                     if Number_Times>0 and Number_Times<=7 and compress_start==0:
                                                         save_01=Calculus[:Times2+3]+Calculus[Times2+4:80]
                                                         Equal_Not_compress=format(Number_Times,'03b')
@@ -247,7 +247,7 @@ class encypthion_class:
                                                 while Times4!=156:
                                                     Times4+=4
                                                     if Times3>=80 and Times4>=80 and Calculus[Times3:Times3+4]!=Calculus[Times4:Times4+4] and Times4!=Times3:
-                                                        Number_Times=(Times3//4)-(Times4//4)
+                                                        Number_Times=((Times3//4)-(Times4//4))-1
                                                         Change=Calculus[Times3:Times3+4]
                                                         Equal_Not_compress=format(Number_Times,'04b')
                                                         if Calculus[Times4:Times4+4]==Equal_Not_compress and Calculus[Times4+4:Times4+5]==Equal_Not_compress[3:4]:
@@ -261,7 +261,7 @@ class encypthion_class:
                                                             compress5=1
                                                             compress7=1
                                                     if Times3>=80 and Times4>=80 and Calculus[Times3:Times3+4]==Calculus[Times4:Times4+4] and Times4!=Times3:
-                                                        Number_Times2=(Times3//4)-(Times4//4)
+                                                        Number_Times2=((Times3//4)-(Times4//4))-1
                                                         if Number_Times2>0 and Number_Times2<=7 and compress_start2==0 and compress2==1:
                                                             save_03=Calculus[80:Times4+3]+Calculus[Times4+4:]
                                                             Equal_Not_compress=format(Number_Times2,'03b')
@@ -287,7 +287,8 @@ class encypthion_class:
                                             size_data12=size_data12+save_04
                                             #print(save_04)
 
-                                    size_data2=size_data12
+                                    size_data2=size_data12[::-1]
+                                    
                                     
                                 
                                 size_data11="1"+size_data12
